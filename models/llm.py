ca014840f1ab642d31be4dddf79677e0c31bbf34
Vamsi Mohan Bangale <130565843+vamsi652@users.noreply.github.com>
2025-09-09 16:29:51 +0530

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -3,11 +3,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from config.config import GOOGLE_API_KEY
 
 def get_chatgemini_model():
-    """Initialize and return the Google Gemini chat model"""
     try:
         gemini_model = ChatGoogleGenerativeAI(
             api_key=GOOGLE_API_KEY,
-            model="gemini-1.5-flash",  # Use the correct model name
+            model="gemini-1.5-flash",
         )
         return gemini_model
     except Exception as e:
